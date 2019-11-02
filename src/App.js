f019380c3f85af9b8c80433c30b62bedf4572ee3
Andrew Zamora <andrewmichaelzamora@gmail.com>
2019-11-02 22:37:56 +0900

    Added add and equate functions

--- src/App.js
+++ src/App.js
@@ -1,29 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
-const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-const numButtons = numbers.map((num, i) => {
-  return (
-    <button id={num} className={`numbers-${i} ${num} pad-btn`} style={{gridArea:num}} key={num}>{i}</button>
-  );
-});
 
-function App() {
-  return (
-    <div className="App">
-      <div id="display" className="display">
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: 0,
+      output: 0,
+    };
+  }
+  onChange = event => {
+    this.setState({ input: event.target.value });
+  }
+  inputNum = num => {
+    this.setState({ output: num });
+  }
+  add = () => {
+    this.setState(prevState => ({
+      input: prevState + this.state.input
+    }));
+  }
+  equate = () => {
+    this.setState({
+      output: this.state.input
+    });
+  }
+  createNumPad = (func) => {
+    const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    return numbers.map((num, i) => {
+      return (
+        <button
+          id={num}
+          className={`numbers-${i} ${num} pad-btn`}
+          style={{ gridArea: num }}
+          key={num}
+          onClick={() => func(i)}>
+          {i}
+        </button>
+      );
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <input
+          id="display"
+          className="display"
+          type="text"
+          value={this.state.output}
+          onChange={() => this.onChange}
+        />
+        <div className="pad">
+          {this.createNumPad(this.inputNum)}
+          <button className="decimal pad-btn" id="decimal" >.</button>
+          <button className="equals pad-btn" id="equals" onClick={()=>this.equate} >=</button>
+          <button className="clear pad-btn" id="clear" >CE</button>
+          <button className="divide operators pad-btn" id="divide">&divide;</button>
+          <button className="multiply operators pad-btn" id="multiply">&times;</button>
+          <button className="subtract operators pad-btn" id="subtract">-</button>
+          <button className="add operators pad-btn" id="add" onClick={()=> this.add}>+</button>
+        </div>
       </div>
-      <div className="pad">
-        {numButtons}
-        <button className="decimal pad-btn" id="decimal" >.</button>
-        <button className="equals pad-btn" id="equals" >=</button>
-        <button className="clear pad-btn" id="clear" >CE</button>
-        <button className="divide operators pad-btn" id="divide">&divide;</button>
-        <button className="multiply operators pad-btn" id="multiply">&times;</button>
-        <button className="subtract operators pad-btn" id="subtract">-</button>
-        <button className="add operators pad-btn" id="add">+</button>
-      </div>
-    </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
