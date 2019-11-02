8e829ef735c5e641fe1722f4f02ba6ad01cee689
Andrew Zamora <andrewmichaelzamora@gmail.com>
2019-11-03 00:30:27 +0900

    Added calculate function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react'
 import './App.css';
 
+const calculate = array => {
+  const operators = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+  }
+  let result;
+  let currentOp;
+  array.forEach(numOrOp => {
+    if (!result) {
+      result = numOrOp;
+    }
+    if (currentOp && typeof numOrOp === "number") {
+      result = operators[currentOp](result, numOrOp);
+    }
+    if (typeof numOrOp === "string") {
+      currentOp = numOrOp;
+    }
+  });
+  return result;
+}
 export class App extends Component {
   constructor(props) {
     super(props);
@@ -52,13 +74,14 @@ export class App extends Component {
         />
         <div className="pad">
           {this.createNumPad(this.inputNum)}
+          {"TEST " + calculate([3, "*","-", 3])}
           <button className="decimal pad-btn" id="decimal" >.</button>
-          <button className="equals pad-btn" id="equals" onClick={()=>this.equate} >=</button>
+          <button className="equals pad-btn" id="equals" onClick={() => this.equate} >=</button>
           <button className="clear pad-btn" id="clear" >CE</button>
           <button className="divide operators pad-btn" id="divide">&divide;</button>
           <button className="multiply operators pad-btn" id="multiply">&times;</button>
           <button className="subtract operators pad-btn" id="subtract">-</button>
-          <button className="add operators pad-btn" id="add" onClick={()=> this.add}>+</button>
+          <button className="add operators pad-btn" id="add" onClick={() => this.add}>+</button>
         </div>
       </div>
     )
